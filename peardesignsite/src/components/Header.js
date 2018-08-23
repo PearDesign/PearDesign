6c35b8ee262740ec18016da69a623a20e388ff79
mdh <me@michaeldharrington.com>
2018-08-22 21:14:50 -0400

    Buttons etc

--- peardesignsite/src/components/Header.js
+++ peardesignsite/src/components/Header.js
@@ -4,10 +4,12 @@ export class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <div className="Home__Hero">
-          <h1>Pear Design is a full-service agency of developers, designers, and digital storytellers.</h1>
+        <div className="Home__hero">
+          <h1 className="Home__heroHeadline">Pear Design is a full-service agency of developers, designers, and digital storytellers.</h1>
+          <p className="Home__heroText">Here's a bit of sample text here.</p>
+          <button className="button__work--header">See our work</button>
         </div>
-        
+
 
       </div>
     );
